e91ec5a687eb4ceae8cafa1fbb5ee0964ae06c61
Thynguyen252 <hoangbaothy.nguyen@sjsu.edu>
2022-05-11 00:41:50 -0700

    basic construction

---
+++ sketch.js
@@ -0,0 +1,14 @@
+ function preload(){
+   corgi = loadImage('assets/corgi.png');
+   sign = loadImage('assets/sign.png');
+  oggy = loadImage('assets/oggy.png');
+ }
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
